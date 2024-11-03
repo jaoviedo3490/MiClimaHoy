248c4fad39ff2547e8c5bccaa5a77424abc3c184
Alex20613 <78511888+Alex20613@users.noreply.github.com>
2024-11-03 13:00:51 -0500

    Se agregan iconos para las imagenes , asi como la visualizacion de la ciudad donde se encuentra el usuario , se modifica la estructura del componente targeta para poder crear el componente de manera generica para mas datos.

--- src/componentes/targeta.jsx
+++ src/componentes/targeta.jsx
@@ -1,67 +1,76 @@
 import { useContext, useState } from "react";
 import { DataContext } from "../Context/MetricsContext";
-const Target = () => {
-
-  const data = {
-    "day": {
-      "day-sunny": "/day-sunny.png", "day-Light-rain-shower": "/day-light-rain.png",
-      "day-Freezing-fog": "/day-Freezing-fog.png", "day-Overcast": "/day-Overcast.png",
-      "day-Heavy-snow":"/day-Heavy-snow.png","day-Partly-cloudy":"/day-Partly-Cloudy.png"
-    },
-    "nigth": {
-      "nigth-Clear": "/nigth-clear.png", "nigth-Patchy-rain-nearby": "/nigth-Patchy-rain-nearby.png",
-      "nigth-Light-rain-shower": "Light-rain-shower.png", "nigth-Partly-cloudy": "/nigth-Partly-Cloudy.png",
-      "nigth-Overcast": "/nigth-Overcast.png", "nigth-Mist": "/nigth-Mist.png",
-      "nigth-Patchy-light-drizzle":"/nigth-Patchy-light-drizzle.png"
-    }
-  }
+import icon from "./dataJson/icon.json";
+const Target = (props) => {
+  const { climateAlert, setClimateAlert } = useContext(DataContext);
+  const [typeTarget, setTypeTarget] = useState(props.tipo);
   const text_alert = {
-    "nubosidad":[{"success":[0,25],"info":[26,50],"warning":[51,75],"danger":[76,100]}],
-    "humedad":[{"success":[0,25],"info":[26,50],"warning":[51,75],"danger":[76,100]}],
-    "visibilidad":[{"success":[11,50],"info":[6,10],"warning":[1,5],"danger":[0,1]}]
+    "nubosidad": [{ "success": [0, 25], "info": [26, 50], "warning": [51, 75], "danger": [76, 100] }],
+    "humedad": [{ "success": [0, 25], "info": [26, 50], "warning": [51, 75], "danger": [76, 100] }],
+    "visibilidad": [{ "success": [11, 50], "info": [6, 10], "warning": [1, 5], "danger": [0, 1] }]
   }
+
+
 
   const getAlert = (value, tipo) => {
     let alertType = 0;
 
     for (const propiedad in text_alert) {
-     
-        const valoresPropiedad = text_alert[propiedad][0];
-        //alert("este es el nivel: "+valoresPropiedad)
-        for (const nivel in valoresPropiedad) {
-           
-            const valoresNivel = valoresPropiedad[nivel];
-            const a = valoresNivel.find((element) => (parseInt(value) >= valoresNivel[0]) && (parseInt(value) <= valoresNivel[1]));
-            if (a !== undefined && propiedad === tipo) {
-                alertType = nivel; 
-                break;
-            }
+
+      const valoresPropiedad = text_alert[propiedad][0];
+      for (const nivel in valoresPropiedad) {
+
+        const valoresNivel = valoresPropiedad[nivel];
+        const a = valoresNivel.find((element) => (parseInt(value) >= valoresNivel[0]) && (parseInt(value) <= valoresNivel[1]));
+        if (a !== undefined && propiedad === tipo) {
+          alertType = nivel;
+          break;
         }
+      }
     }
     return alertType;
-}
+  }
+
+  switch (typeTarget) {
+    case "pronostico":
+      let climate = climateAlert.current.condition.text;
+      climate = climate.replaceAll(" ", "-");
+
+      const is_day = (climateAlert.current.is_day === 0) ? "nigth" : "day";
+      const alertaNubosidad = getAlert(climateAlert.current.cloud, "nubosidad");
+      const alertaHumedad = getAlert(climateAlert.current.humidity, "humedad");
+      const alertaVisibilidad = getAlert(climateAlert.current.vis_km, "visibilidad");
+      let iconjson = icon['icon'][is_day][`${is_day}-${climate}`];
+      
+      
+
+      return (
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={iconjson} className="card-img-top" alt="Imagen" />
+          <div className="card-body">
+            <h5 className="card-title">{`${is_day}: ${climateAlert.current.condition.text}`}</h5>
+            <p className={`card-title text-${alertaNubosidad}`}><strong>Nubosidad: {climateAlert.current.cloud}%</strong></p>
+            <p className={`card-title text-${alertaHumedad}`}><strong>Humedad: {climateAlert.current.humidity}%</strong></p>
+            <p className={`card-title text-${alertaVisibilidad}`}><strong>Visibilidad: {climateAlert.current.vis_km} Kilometros</strong></p>
+          </div>
+        </div>
+      );
+      break;
+
+    case "calidad-aire":
+      return(
+        <div className="card" style={{width: '18rem'}}>
+            <img src="..." className="card-img-top" alt="calidad-aire"/>
+            <div className="card-title">{`${climateAlert.current.air_quality.co}`}
+
+            </div>
+        </div>
+      );
+       break;
+    default: break;
+  }
 
 
-  const { climateAlert, setClimateAlert } = useContext(DataContext);
-  const is_day = (climateAlert.is_day === 0) ? "nigth" : "day";
-  const alertaNubosidad = getAlert(climateAlert.cloud,"nubosidad");
-  const alertaHumedad = getAlert(climateAlert.humidity,"humedad");
-  const alertaVisibilidad = getAlert(climateAlert.vis_km,"visibilidad")
-  let climate = climateAlert.condition.text;
-  climate = climate.replaceAll(" ", "-");
-
-  let icon = data[is_day][`${is_day}-${climate}`]
-  return (
-    <div className="card" style={{ width: '18rem' }}>
-      <img src={icon} className="card-img-top" alt="Imagen" />
-      <div className="card-body">
-        <h5 className="card-title">{`${is_day}: ${climateAlert.condition.text}`}</h5>
-        <p className={`card-title text-${alertaNubosidad}`}><strong>Porcentaje de Nubosidad: {climateAlert.cloud}%</strong></p>
-        <p className={`card-title text-${alertaHumedad}`}><strong>Porcentaje de humedad: {climateAlert.humidity}%</strong></p>
-        <p className={`card-title text-${alertaVisibilidad}`}><strong>Visibilidad: {climateAlert.vis_km} Kilometros</strong></p>
-      </div>
-    </div>
-  );
 }
 export default Target;
 
--- src/componentes/updateDate.jsx
+++ src/componentes/updateDate.jsx
@@ -1,14 +1,28 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../Context/MetricsContext";
-const UpdateDate = () =>{
-const{climateAlert,setClimateAlert} = useContext(DataContext);
+const UpdateDate = () => {
+    const { climateAlert, setClimateAlert } = useContext(DataContext);
+    const [ciudad , setCiudad] = useState('');
+    const [fecha , setFecha] = useState('');
+    
+    useEffect(()=>{
+        setCiudad(climateAlert.location.name);
+        setFecha(climateAlert.current.last_updated);
+    },[climateAlert])
+    return (
 
-return (
-
-<div>
-    <h4 className="text-light">Ultima Fecha de Actualización: {climateAlert.last_updated}</h4>
-</div>
-);
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                     <h5 className="text-light text-start">Ciudad:{ciudad}</h5>
+                </div>
+                <div className="col">
+                    <h5 className="text-light">Ultima Fecha de Actualización: {fecha}</h5>
+                </div>
+            </div>
+           
+        </div>
+    );
 
 }
 export default UpdateDate;
